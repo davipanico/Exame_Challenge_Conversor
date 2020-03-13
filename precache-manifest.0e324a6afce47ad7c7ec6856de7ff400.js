ea01dc297dc522e6350592d8bf2cff87db59b8d5
Davi <davi_head@hotmail.com>
2020-03-12 23:36:18 -0300

    Updates

---
+++ precache-manifest.0e324a6afce47ad7c7ec6856de7ff400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2306fab93da0bb850bce1f2d9cc5af11",
+    "url": "/Exame_Challenge_Conversor/index.html"
+  },
+  {
+    "revision": "aa9a4844a29c87a1228f",
+    "url": "/Exame_Challenge_Conversor/static/css/main.6b7f8979.chunk.css"
+  },
+  {
+    "revision": "7e6347317c4da2e7c6d6",
+    "url": "/Exame_Challenge_Conversor/static/js/2.794bff77.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Exame_Challenge_Conversor/static/js/2.794bff77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa9a4844a29c87a1228f",
+    "url": "/Exame_Challenge_Conversor/static/js/main.e8587034.chunk.js"
+  },
+  {
+    "revision": "ea78e0eb66f2b26122c5",
+    "url": "/Exame_Challenge_Conversor/static/js/runtime-main.7dbaba4d.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Exame_Challenge_Conversor/precache-manifest.0e324a6afce47ad7c7ec6856de7ff400.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Exame_Challenge_Conversor/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
